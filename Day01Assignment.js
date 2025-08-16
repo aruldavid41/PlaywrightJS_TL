241d26f40d8bd826ecb7fee21101bb267d3b771a
aruldavid41 <135032124+aruldavid41@users.noreply.github.com>
2025-08-16 18:09:55 +0530

    Add files via upload

---
+++ Day01Assignment.js
@@ -0,0 +1,41 @@
+var city = "Chennai"
+console.log("I live in " + city)
+
+let marks = 85
+marks + 10
+console.log(marks + 10)
+
+var isWeekend = true
+
+if (isWeekend) {
+
+    var weekend = "relax"
+    console.log("This weekend " + weekend)
+
+} else {
+    console.log("This weekend " + "work")
+}
+
+let futureGoal;
+console.log(futureGoal)
+
+console.log(typeof city)
+console.log(typeof marks)
+console.log(typeof isWeekend)
+console.log(typeof futureGoal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
